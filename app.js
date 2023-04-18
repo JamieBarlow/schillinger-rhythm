9e39baf4a31db3d7bfbeb5b76ae661a15e153c33
Jamie Barlow <jamiebarlow@gmail.com>
2023-04-18 19:18:45 +0100

    Add pulse beat

--- app.js
+++ app.js
@@ -47,9 +47,9 @@ function updateDevices(event) {
 
 
 // instrument parts
-let bd, snare, hh;           // instrument. This serves as a container that will hold a sound source.
-let bdPat, snarePat, hhPat;  // pattern. Will be an array of numbers (1 = on-note, 0 = rest)
-let bdPhrase, snarePhrase, hhPhrase;  // defines how the pattern is interpreted
+let bd, snare, hh, pulse;           // instrument. This serves as a container that will hold a sound source.
+let bdPat, snarePat, hhPat, pulsePat;  // pattern. Will be an array of numbers (1 = on-note, 0 = rest)
+let bdPhrase, snarePhrase, hhPhrase, pulsePhrase;  // defines how the pattern is interpreted
 let drums;  // full drum part. We will attach the phrase to the part, which will serve as our transport to drive the phrase.
 let bpmControl;
 let beatLength;
@@ -59,23 +59,29 @@ let cnv;
 let playhead = [];
 let cursorPos;
 
+pattern = [1, 0, 5, 4, 5];
+
 // p5 sketch setup
 function setup() {
     cnv = createCanvas(320, 60);
     cnv.mousePressed(canvasPressed);
 
-    beatLength = 16;
-    numInstruments = 3;
-    cellWidth = width / beatLength;
-    cursorPos = 0;
-
     hh = loadSound('assets/MPC60/CH 909 A MPC60 07.wav', () => { });  // return to this callback later
     snare = loadSound('assets/MPC60/Snare Wood Tail Short MPC60 13.wav', () => { });  // return to this callback later
     bd = loadSound('assets/MPC60/BD Club Pressure MPC60 11.wav', () => { });  // return to this callback later
+    pulse = loadSound('assets/DMX/CH Acc DMX 21.wav', () => { });
 
-    hhPat = [0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1];
-    snarePat = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0];
-    bdPat = [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0];
+    hhPat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    beatLength = hhPat.length;
+    snarePat = convertPattern(pattern, true);
+    bdPat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    pulsePat = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,];
+
+    // beatLength = 16;
+    numInstruments = 4;
+    cellWidth = width / beatLength;
+    cursorPos = 0;
+
     createPlayhead();
     hhPhrase = new p5.Phrase('hh', (time) => {
         hh.play(time);
@@ -89,12 +95,17 @@ function setup() {
         bd.play(time);
         // console.log(time);
     }, bdPat);
+    pulsePhrase = new p5.Phrase('pulse', (time) => {
+        pulse.play(time);
+        // console.log(time);
+    }, pulsePat);
 
     drums = new p5.Part();
 
     drums.addPhrase(hhPhrase);
     drums.addPhrase(snarePhrase);
     drums.addPhrase(bdPhrase);
+    drums.addPhrase(pulsePhrase);
     drums.addPhrase('seq', sequence, playhead)
 
     bpmControl = createSlider(30, 300, 120, 1);
@@ -221,5 +232,3 @@ function isIrregular(ptn) {
     const ptnLength = ptn.reduce((a, b) => a + b);
     return (ptnLength % 2 !== 0) ? true : false;
 }
-
-convertPattern([1, 0, 2, 4, 6], true);
